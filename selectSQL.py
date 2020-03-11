0ac573bf59cb9d0cafea3898f3ecdfa8f18df4b0
Dreammer12138 <wangshibo142@gmail.com>
2020-03-11 09:03:28 +0800

    update

--- selectSQL.py
+++ selectSQL.py
@@ -1,9 +1,9 @@
-#/usr/bin/python3
+# /usr/bin/python3
 # select SQL
 # author Dreammer12138
-#-------------------+-----------+-----------------------
+# -------------------+-----------+-----------------------
 # Variable 			|Type		|Description			
-#-------------------+-----------+-----------------------
+# -------------------+-----------+-----------------------
 # __SQL				|string		|SQL statement
 # __column 			|list		|column for select
 # __as_column		|list		|as
@@ -13,46 +13,46 @@
 # __init__			|function	|Constructor function
 # where 			|function	|condition function
 # SQL 				|function	|return SQL statement
-#-------------------+-----------+-----------------------
+# -------------------+-----------+-----------------------
 
 class selectSQL:
-	__SQL = "SELECT "
-	__column = []
-	__as_column = []
-	__table_name = ""
-	__distinct = False
-	__where = ""
-	
-	def __init__(self, table_name, attribute, distinct = False):
-		self.__table_name = table_name
-		if isinstance(attribute, str):
-			self.__column.append(attribute)
-		elif isinstance(attribute, list):
-			self.__column += attribute
-		elif isinstance(attribute, dict):
-			for a in attribute:
-				self.__column.append(a)
-				self.__as_column.append(attribute[a])
-		self.__distinct = distinct
+    __SQL = "SELECT "
+    __column = []
+    __as_column = []
+    __table_name = ""
+    __distinct = False
+    __where = ""
 
-	def where(self, wh):
-		self.__where += wh
+    def __init__(self, table_name, attribute, distinct = False):
+        self.__table_name = table_name
+        if isinstance(attribute, str):
+            self.__column.append(attribute)
+        elif isinstance(attribute, list):
+            self.__column += attribute
+        elif isinstance(attribute, dict):
+            for a in attribute:
+                self.__column.append(a)
+                self.__as_column.append(attribute[a])
+        self.__distinct = distinct
 
-	def SQL(self):
-		SQL = self.__SQL
-		if self.__distinct:
-			SQL += "DISTINCT "
-		for index, c in enumerate(self.__column):
-			if index != 0:
-				SQL += ", "
-			SQL += "`{}`".format(c)
-		if self.__as_column:
-			SQL += " AS "
-			for index, a in enumerate(self.__as_column):
-				if index != 0:
-					SQL += ", "
-				SQL += "`{}`".format(a)
-		SQL += " FROM `{}`".format(self.__table_name)
-		if self.__where != "":
-			SQL += self.__where
-		return SQL
+    def where(self, wh):
+        self.__where += wh
+
+    def SQL(self):
+        SQL = self.__SQL
+        if self.__distinct:
+            SQL += "DISTINCT "
+        for index, c in enumerate(self.__column):
+            if index != 0:
+                SQL += ", "
+            SQL += "`{}`".format(c)
+        if self.__as_column:
+            SQL += " AS "
+            for index, a in enumerate(self.__as_column):
+                if index != 0:
+                    SQL += ", "
+                SQL += "`{}`".format(a)
+        SQL += " FROM `{}`".format(self.__table_name)
+        if self.__where != "":
+            SQL += self.__where
+        return SQL
